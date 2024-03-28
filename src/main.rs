4447d678aff0b8a454f2d58d39d3c816d0d808d2
Hendrik Obermayer <hendriko@amazon.de>
2024-03-28 17:41:06 +0100

    Changes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use salvo::prelude::*;
 use salvo::http::StatusCode;
-use rand::*;
 use cookie::Cookie;
+
+use std::default::Default;
+
+// Implement this:
+// https://github.com/ramosbugs/openidconnect-rs/blob/main/examples/gitlab.rs
 
 // Examples: https://github.com/salvo-rs/salvo/blob/main/examples/csrf-cookie-store/src/main.rs
 
 fn get_auth_cookie(cookie_string: &str) -> String{
-
     let mut cookie_value = None;
     for cookie in Cookie::split_parse(cookie_string) {
         let cookie = cookie.unwrap();
@@ -22,29 +25,43 @@ fn get_auth_cookie(cookie_string: &str) -> String{
 
 #[handler]
 async fn authorizer(req: &mut Request, res: &mut Response, _depot: &mut Depot) {
-    let mut rng = rand::thread_rng();
-    let random = rng.gen_range(0..3);
 
     // let user = depot.get::<&str>("traefik_oidc").copied().unwrap_or("");
     // println!("JWT: {}", user);
 
+    ////////////////////////////////
+    // HEADERS
     for (key, value) in req.headers().into_iter() {
         println!("{}: {}", key.as_str(), value.to_str().unwrap_or_default());
     }
+    ////////////////////////////////
 
 
+    ////////////////////////////////
+    // JWT
     let jwt_cookie = get_auth_cookie(req.headers().get("cookie").expect("").to_str().unwrap_or_default());
-
     println!("JWT: {}", jwt_cookie);
-    
-    // X-Forwarded-Host
-    // X-Forwarded-Proto
-    // X-Forwarded-Uri
-    // X-Forwarded-For
 
-    // res.headers_mut()
-    // .insert(header::SERVER, HeaderValue::from_static("Salvo"));
+    let headerx =  match req.headers().get("X-Forwarded-Host") {
+        Some(header) => header.to_str().unwrap_or(""),
+        None => "",
+    };
 
+    println!("X-Forwarded-Host: {}", headerx);
+
+    // Refactor these lines
+    // println!("X-Forwarded-Host: {}", req.headers().get("X-Forwarded-Host").expect("").to_str().unwrap_or(""));
+    // println!("X-Forwarded-Proto: {}", req.headers().get("X-Forwarded-Proto").expect("").to_str().unwrap_or(""));
+    // println!("X-Forwarded-Uri: {}", req.headers().get("X-Forwarded-Uri").expect("").to_str().unwrap_or(""));
+    // println!("X-Forwarded-For: {}", req.headers().get("X-Forwarded-For").expect("").to_str().unwrap_or(""));
+    // println!("-------");
+
+    // res.headers_mut().insert(header::SERVER, HeaderValue::from_static("Salvo"));
+
+
+
+    ////////////////
+    // Response
     if jwt_cookie == "0" {
         res.status_code(StatusCode::UNAUTHORIZED).render(Text::Plain("NOK"));
     } else if jwt_cookie == "1" {
