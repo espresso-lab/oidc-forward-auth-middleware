46a3deba22000765f2ed39895653e00be642ea8a
Jannick Viets <jannick@viets.email>
2024-03-31 15:17:37 +0200

    Code refactored

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,11 @@
-use cookie::Cookie;
-use openidconnect::core::{
-    CoreClient, CoreIdTokenClaims, CoreIdTokenVerifier, CoreProviderMetadata, CoreResponseType,
-};
+use openidconnect::core::{CoreClient, CoreProviderMetadata, CoreResponseType};
 use openidconnect::reqwest::http_client;
 use openidconnect::{
-    AuthenticationFlow, AuthorizationCode, ClientId, ClientSecret, CsrfToken, IssuerUrl, Nonce,
-    OAuth2TokenResponse, RedirectUrl, Scope,
+    AuthenticationFlow, ClientId, ClientSecret, CsrfToken, IssuerUrl, Nonce, RedirectUrl, Scope,
 };
-use querystring::querify;
 use salvo::http::StatusCode;
 use salvo::prelude::*;
 use std::env;
-
-fn get_env(key: &str, default: Option<&str>) -> String {
-    env::var(key).unwrap_or_else(|_| default.unwrap_or("").to_owned())
-}
-
-// Implement this:
-// https://github.com/ramosbugs/openidconnect-rs/blob/main/examples/gitlab.rs
-
-// Examples: https://github.com/salvo-rs/salvo/blob/main/examples/csrf-cookie-store/src/main.rs
-
-fn handle_error<T: std::error::Error>(fail: &T, msg: &'static str) {
-    let mut err_msg = format!("ERROR: {}", msg);
-    let mut cur_fail: Option<&dyn std::error::Error> = Some(fail);
-    while let Some(cause) = cur_fail {
-        err_msg += &format!("\ncaused by: {}", cause);
-        cur_fail = cause.source();
-    }
-    println!("{}", err_msg);
-}
 
 #[derive(Clone)]
 struct OIDCProvider {
@@ -39,20 +15,26 @@ struct OIDCProvider {
     issuer_url: IssuerUrl,
 }
 
+fn get_env(key: &str, default: Option<&str>) -> String {
+    env::var(key).unwrap_or_else(|_| default.unwrap_or("").to_owned())
+}
+
 fn get_oidc_providers() -> Vec<OIDCProvider> {
-    let binding = get_env("AUTH_HOSTNAMES", None);
-    let hostnames = binding.split(",").collect::<Vec<_>>();
-    let binding = get_env("AUTH_PROVIDER_URLS", None);
-    let urls = binding.split(",").collect::<Vec<_>>();
-    let binding = get_env("AUTH_CLIENT_IDS", None);
-    let clients = binding.split(",").collect::<Vec<_>>();
-    let binding = get_env("AUTH_SECRETS", None);
-    let secrets = binding.split(",").collect::<Vec<_>>();
+    let hostnames = get_env("AUTH_HOSTNAMES", None);
+    let provider_urls = get_env("AUTH_PROVIDER_URLS", None);
+    let client_ids = get_env("AUTH_CLIENT_IDS", None);
+    let secrets = get_env("AUTH_SECRETS", None);
+    let (hostnames, urls, clients, secrets) = (
+        hostnames.split(",").collect::<Vec<_>>(),
+        provider_urls.split(",").collect::<Vec<_>>(),
+        client_ids.split(",").collect::<Vec<_>>(),
+        secrets.split(",").collect::<Vec<_>>(),
+    );
 
     urls.iter()
         .enumerate()
         .map(|(index, url)| OIDCProvider {
-            hostname: hostnames.get(index).unwrap().to_string(), // TODO: to lowercase
+            hostname: hostnames.get(index).unwrap().to_string().to_lowercase(),
             client_id: ClientId::new(clients.get(index).unwrap().to_string()),
             client_secret: ClientSecret::new(secrets.get(index).unwrap().to_string()),
             issuer_url: IssuerUrl::new(url.to_string()).expect("Invalid issuer URL"),
@@ -60,7 +42,6 @@ fn get_oidc_providers() -> Vec<OIDCProvider> {
         .collect()
 }
 
-// TODO: Get the credentials from the provided hostname
 fn get_oidc_provider_for_hostname(hostname: String) -> Option<OIDCProvider> {
     get_oidc_providers()
         .iter()
@@ -68,22 +49,8 @@ fn get_oidc_provider_for_hostname(hostname: String) -> Option<OIDCProvider> {
         .cloned()
 }
 
-fn get_auth_cookie(cookie_string: &str) -> String {
-    let mut cookie_value = None;
-    for cookie in Cookie::split_parse(cookie_string) {
-        let cookie = cookie.unwrap();
-
-        if cookie.name() == get_env("AUTH_COOKIE_NAME", Some("forward_auth")) {
-            cookie_value = Some(cookie.value().to_string());
-            break;
-        }
-    }
-    cookie_value.unwrap_or_default()
-}
-
 #[handler]
 async fn forward_auth_handler(req: &mut Request, res: &mut Response) {
-    // TODO: to lowercase
     let hostname = req
         .headers()
         .get("X-Forwarded-Host")
@@ -91,8 +58,6 @@ async fn forward_auth_handler(req: &mut Request, res: &mut Response) {
         .to_str()
         .unwrap_or("")
         .to_string();
-
-    // Get the OIDC Provider
     let oidc_provider = match get_oidc_provider_for_hostname(hostname.clone()) {
         Some(val) => val,
         None => {
@@ -101,38 +66,20 @@ async fn forward_auth_handler(req: &mut Request, res: &mut Response) {
                 .render(Text::Plain("No OIDC provider known for hostname."))
         }
     };
-    let issuer_url = oidc_provider.issuer_url.url().to_string();
-    let client_id = oidc_provider.client_id.to_string();
 
-    // Load this on start and NOT per request
-    let provider_metadata = tokio::task::spawn_blocking(move || {
-        let my_issuer_url = IssuerUrl::new(issuer_url).expect("Invalid issuer URL");
-
-        CoreProviderMetadata::discover(&my_issuer_url, http_client).unwrap_or_else(|err| {
-            handle_error(&err, "Failed to discover OpenID Provider");
-            unreachable!();
-        })
-    })
-    .await
-    .unwrap();
-
-    let hostname2 = hostname.clone();
-
-    // Set up the config for the GitLab OAuth2 process.
+    let provider_metadata =
+        CoreProviderMetadata::discover(&oidc_provider.issuer_url, http_client).unwrap();
     let client = CoreClient::from_provider_metadata(
         provider_metadata,
         oidc_provider.client_id,
         Some(oidc_provider.client_secret),
     )
     .set_redirect_uri(
-        RedirectUrl::new(format!("https://{}/auth_callback", hostname2).to_string())
+        RedirectUrl::new(format!("https://{}/auth_callback", hostname.clone()).to_string())
             .expect("Invalid redirect URL"),
     );
 
-    println!("4 Hallo Welt");
-
-    // Generate the authorization URL to which we'll redirect the user.
-    let (authorize_url, csrf_state, nonce) = client
+    let (authorize_url, _, _) = client
         .authorize_url(
             AuthenticationFlow::<CoreResponseType>::AuthorizationCode,
             CsrfToken::new_random,
@@ -142,131 +89,7 @@ async fn forward_auth_handler(req: &mut Request, res: &mut Response) {
         .add_scope(Scope::new("profile".to_string()))
         .url();
 
-    println!("Open this URL in your browser:\n{}\n", authorize_url);
-
-    // TODO: csrf_state und nonce wegspeichern
-
-    // https://example.com/oauth_callback?state=eyJzdGF0ZSI6IlYxZFFUa0pYTUVjMFJVVTFNVmhNVURFek1VRlVWVE5RUnpwaVJWbHJVelpFVmxSTFJuZzVSV05LV0dsSlRscFNSRWRYWTNCS1REbExRbUZqTlVOalExZE5ZME56UFE9PSIsImlkcF9uYW1lIjoia2FuaWRtIn0%3D&code=gAAAAABmBzT5hPIYqxegNadrOyAUX3Wldm_ECT_OCztlX3PKJZ5OBKLPI5cPxgaDKhR9ci7DBvGHz6uWVWFXBeSysDL215eEpPNHFbS9-MGat9HjSRfwjeStdoEp46Y3YZ-i_GyszW1EhUgMO4S5uyRG8zty1JKnREqtAldhlcNqCdxXSZZw-4d7quAr_bPaywON1I6lEur7RMthEdM4Sf1_h_khWb08N-KFyWeVV50myHqHAAuBuxvDte416XTMod7VCabkq9iI6TGmYzTFHQwI6r7m-oBoh_zP90_E9vH9V-JxY8JqSkW1FLiMGaKDz1C_MAhPI3MwrBt-_lf1ncte0Z3UAn3I3w07BmMe2h3CWrz7IzV8kZ2D_vrAvlBezo_P9EgUCxvuP8KVYTGhULo6sNHGAMirfQ%3D%3D
-
-    // https://example.com/auth_callback?state=Us1y9ilun1ONXpjODHMNXQ&code=gAAAAABmCKNmTHhNQFxPDKDy9Z2Sc6An37k6SgI7ZpJjX7oel4IyrUXz1ULZfn3UqCCj5C2ctD2Vd7V75mxpKjUcRcf_U0-__KKsS-YKy2-zBZtgjp76Pt6hbtG-OKsP_naQ4_n5Qf-THUCFqBRMD-2X2FuEk5ZrxxrktMJryXdu0qPBFwtSE9s5s11iwJjP2r750G6z72MV7L-hx1VfKFDinecthcB3KVHM0Ha-BxintZvMv9yoNvmyyb_ZK4_Dmc_HdxXS4i-Bz-oc6y05zwvF8Y7pCTmHF91WB0ib5v6dmgeAVHFRI7ePMQ-k_8qE59qTFGHTuh72b5tZ82LWiWi9XOJFTwYKEi7hf-nkU--XheK5l_fWwz-x45Q6SQWQgCKgSfuMdalXBoB-hN2-FFgQF6BLjnLZEOR5qihpiCTBZuO4e0_xHFg%3D
-
-    // println!("X-Forwarded-Uri: {}", req.headers().get("X-Forwarded-Uri").expect("").to_str().unwrap_or(""));
-
-    let uri = req
-        .headers()
-        .get("X-Forwarded-Uri")
-        .expect("")
-        .to_str()
-        .unwrap_or("");
-    println!("URI: {}", uri);
-    let binding = uri
-        .replace("/auth_callback?", "")
-        .replace("auth_callback?", "");
-    let query_params = querify(&binding);
-
-    println!("{:?}", query_params);
-
-    let str_code = match query_params.iter().filter(|x| x.0 == "code").next() {
-        Some(str) => str.1,
-        None => "",
-    };
-
-    let str_state = match query_params.iter().filter(|x| x.0 == "state").next() {
-        Some(str) => str.1,
-        None => "",
-    };
-
-    // Verify stuff
-    if !str_code.is_empty() && !str_state.is_empty() {
-        println!("Got some code and state yihaa");
-
-        let code = AuthorizationCode::new(str_code.to_string());
-        let state = CsrfToken::new(str_state.to_string());
-
-        println!("CODE:\n{}\n", code.secret());
-        println!(
-            "STATE:\n{} (expected `{}`)\n",
-            state.secret(),
-            csrf_state.secret()
-        );
-
-        // Exchange the code with a token.
-        let token_response = client
-            .exchange_code(code)
-            .request(http_client)
-            .unwrap_or_else(|err| {
-                handle_error(&err, "Failed to contact token endpoint");
-                unreachable!();
-            });
-
-        println!(
-            "access token:\n{}\n",
-            token_response.access_token().secret()
-        );
-        println!("SCOPES: {:?}", token_response.scopes());
-
-        let id_token_verifier: CoreIdTokenVerifier = client.id_token_verifier();
-        let id_token_claims: &CoreIdTokenClaims = token_response
-            .extra_fields()
-            .id_token()
-            .expect("Server did not return an ID token")
-            .claims(&id_token_verifier, &nonce)
-            .unwrap_or_else(|err| {
-                handle_error(&err, "Failed to verify ID token");
-                unreachable!();
-            });
-        println!("ID token: {:?}\n", id_token_claims);
-
-        res.status_code(StatusCode::NO_CONTENT);
-    }
-
     res.render(Redirect::temporary(authorize_url.to_string()));
-
-    ////////////////////////////////
-    // HEADERS
-    // for (key, value) in req.headers().into_iter() {
-    //     println!("{}: {}", key.as_str(), value.to_str().unwrap_or_default());
-    // }
-    ////////////////////////////////
-
-    ////////////////////////////////
-    // JWT
-
-    // let jwt_cookie = get_auth_cookie(
-    //     req.headers()
-    //         .get("cookie")
-    //         .expect("")
-    //         .to_str()
-    //         .unwrap_or_default(),
-    // );
-    // println!("JWT: {}", jwt_cookie);
-
-    // let headerx = match req.headers().get("X-Forwarded-Host") {
-    //     Some(header) => header.to_str().unwrap_or(""),
-    //     None => "",
-    // };
-
-    // println!("X-Forwarded-Host: {}", headerx);
-
-    // Refactor these lines
-    // println!("X-Forwarded-Host: {}", req.headers().get("X-Forwarded-Host").expect("").to_str().unwrap_or(""));
-    // println!("X-Forwarded-Proto: {}", req.headers().get("X-Forwarded-Proto").expect("").to_str().unwrap_or(""));
-    // println!("X-Forwarded-Uri: {}", req.headers().get("X-Forwarded-Uri").expect("").to_str().unwrap_or(""));
-    // println!("X-Forwarded-For: {}", req.headers().get("X-Forwarded-For").expect("").to_str().unwrap_or(""));
-    // println!("-------");
-
-    // res.headers_mut().insert(header::SERVER, HeaderValue::from_static("Salvo"));
-
-    ////////////////
-    // Response
-    // if jwt_cookie == "0" {
-    //     res.status_code(StatusCode::UNAUTHORIZED)
-    //         .render(Text::Plain("NOK"));
-    // } else if jwt_cookie == "1" {
-    //     res.status_code(StatusCode::OK).render(Text::Plain("OK"));
-    // } else {
-    //     res.render(Redirect::temporary(authorize_url.to_string()));
-    // }
 }
 
 #[handler]
@@ -276,8 +99,6 @@ async fn status(res: &mut Response) {
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt().init();
-
     let router = Router::new()
         .push(Router::with_path("/status").get(status))
         .push(Router::with_path("/verify").get(forward_auth_handler));
